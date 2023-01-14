86da44ca077a7b058f12e815623e833094bdb424
CAHOn3R <74197777+CAHOn3R@users.noreply.github.com>
2023-01-14 18:40:42 +0100

    Update ip.sh

--- polybar/ip.sh
+++ polybar/ip.sh
@@ -4,7 +4,7 @@ IFACE=$(/usr/sbin/ifconfig | grep tun0 | awk '{print $1}' | tr -d ':')
  
 if [ "$IFACE" = "tun0" ]; then
     echo "  %{F#1bbf3e} %{F#ffffff}$(/usr/sbin/ifconfig tun0 | grep "inet " | awk '{print $2}')%{u-}  "
-    echo "$(/usr/sbin/ifconfig tun0 | grep "inet " | awk '{print $2}')" > /home/cahoner/.config/polybar/HackerDesktop/copyTun0.txt
+    echo "$(/usr/sbin/ifconfig tun0 | grep "inet " | awk '{print $2}')" > ~/.config/polybar/HackerDesktop/copyTun0.txt
 else
-    echo "" #echo "  %{F#1bbf3e} %{u-}Disconnected  "
+    echo ""
 fi
