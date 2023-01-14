7b5ccdbfd67ae71fa85fbb3850d55e52e6ed770d
CAHOn3R <74197777+CAHOn3R@users.noreply.github.com>
2023-01-14 18:41:00 +0100

    Update ipens33.sh

--- polybar/ipens33.sh
+++ polybar/ipens33.sh
@@ -4,7 +4,7 @@ IFACE=$(/usr/sbin/ifconfig | grep ens33 | awk '{print $1}' | tr -d ':')
  
 if [ "$IFACE" = "ens33" ]; then
     echo "  %{F#46e0ce}泌 %{F#ffffff}$(/usr/sbin/ifconfig ens33 | grep "inet " | awk '{print $2}')%{u-}  "
-    echo "$(/usr/sbin/ifconfig ens33 | grep "inet " | awk '{print $2}')" > /home/cahoner/.config/polybar/HackerDesktop/copyEns33.txt
+    echo "$(/usr/sbin/ifconfig ens33 | grep "inet " | awk '{print $2}')" > ~/.config/polybar/HackerDesktop/copyEns33.txt
 else
     echo "%{F#1bbf3e}泌%{u-} Disconnected"
 fi
